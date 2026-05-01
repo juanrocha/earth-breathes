433f851b96e004f39feb08e862b789feb47c7d2a
juanrocha <aguilajk@gmail.com>
2026-05-01 12:00:01 +0200

    quality flags

--- tiling_quality_flags.R
+++ tiling_quality_flags.R
@@ -4,10 +4,42 @@ library(tictoc)
 
 # When working from back up disk:
 # "/Volumes/Data backups/DATA/LP_DAAC_Data_Pool/daac_data_download_r/MOD13A2-061"
+load("data/tiles.Rda")
+
+fl <- tiles |> filter(grid == "h10v08") |> slice(1) |> pull(file)
+
+sd <- sf::gdal_subdatasets(fl)
+# 1: NDVI, 2: EVI
+# 3: VI quality
+# 12: pixel reliability (-1 = NA, 0 = good, 1 = use with caution, 2 = snow/ice, 3 = clouds)
+
+x <- terra::rast(fl)
+lobstr::obj_size(x) # 9.66MB
 
 
-#### Files to read ####
-fls <- dir_ls("/Volumes/Data backups/DATA/LP_DAAC_Data_Pool/daac_data_download_r/MYD13A2-061/") |>
+range(x$`"1 km 16 days NDVI"` )
+range(x$`"1 km 16 days VI Quality"`)
+
+y <- stars::read_stars(sd[[3]])
+z <- terra::rast(sd[[1]])
+
+x$`"1 km 16 days NDVI"`
+
+# A simple multiplication can mask the low values, then this can be exported to GeoTif
+terra::plot(
+    x$`"1 km 16 days NDVI"` * (x$`"1 km 16 days pixel reliability"` == 0)
+    )
+
+# J260501: I can extract both layers, multiply and then mask out the low quality values
+
+terra::plot(z)
+plot(y)
+
+range(z)
+range(as.matrix(z), na.rm = TRUE)
+
+#### Files to read: only if working with external back up disk ####
+fls <- dir_ls("~/Documents/Projects/DATA/LP_DAAC_Data_Pool/daac_data_download_r/MYD13A2-061/") |>
     str_subset(pattern = "\\.hdf$") # aqua
 fls2 <- dir_ls("/Volumes/Data backups/DATA/LP_DAAC_Data_Pool/daac_data_download_r/MOD13A2-061/") |>
     str_subset(pattern = "\\.hdf$") # terra
